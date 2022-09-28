401291abb34a7e0ad95b4c34ffbe637900f6be50
PrinceManfred <development@castleofotranto.net>
2022-09-28 17:02:03 -0500

    Added attributes and moved source generation into static class

---
+++ Examples/BasicAPI/Features/ServiceInjection.cs
@@ -0,0 +1,10 @@
+﻿using CQRSToolkit.DependencyInjection.Generator.Attributes;
+
+namespace BasicAPI.Features
+{
+    public static partial class ServiceInjection
+    {
+        [ServiceInjectionPoint]
+        public static partial IServiceCollection AddThemBois(this IServiceCollection services, ServiceLifetime defaultLifetime = ServiceLifetime.Scoped);
+    }
+}

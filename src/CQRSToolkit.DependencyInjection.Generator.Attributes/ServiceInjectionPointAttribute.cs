cebb196c6be2eda3d505ac8b15aab368eff6c8c3
PrinceManfred <development@castleofotranto.net>
2022-09-29 15:12:25 -0500

    Customization options, diagnostic reporting. (#2)
    * Added config options for generator
    
    * Added attributes and moved source generation into static class
    
    * Changed LangVersion to be properly .net standard 2.0 compatible
    
    * Moved package properties to a unified location
    
    * Added support for different parameter names. Added diagnotics.

---
+++ src/CQRSToolkit.DependencyInjection.Generator.Attributes/ServiceInjectionPointAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace CQRSToolkit.DependencyInjection.Generator.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class ServiceInjectionPointAttribute : Attribute { }
+}
